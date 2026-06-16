61bab8842fd8ed63e6a2d0dd65809459f001419f
bicky raj kayastha <bickyrajkayastha@gmail.com>
2026-06-16 14:27:15 +0200

    updates

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,33 @@ import { Server } from "socket.io";
 import {db} from "./db/db";
 import roomRoutes from "./routes/room.routes";
 import {SocketService} from "./services/socket.service";
+import nodemailer from "nodemailer";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", roomRoutes);
+
+const transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "1e562466c6c690",
+        pass: "03b4aca9e2cdd7",
+    },
+});
+
+app.post("/api/send-email", async (req, res) => {
+    const {email, body} = req.body;
+    await transporter.sendMail({
+        from: email,
+        to: "bickyrajkayastha@gmail.com",
+        subject: "portfolio message",
+        text: body,
+    });
+
+    res.send("Email sent");
+});
 
 const server = http.createServer(app);
 
